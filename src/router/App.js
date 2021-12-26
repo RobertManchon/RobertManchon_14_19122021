46dca13cf07eccbe934e2339c63be26e4a273861
RManchon <erasme666@gmail.com>
2021-12-26 09:33:23 +0100

    feat: add context & reducer

--- src/router/App.js
+++ src/router/App.js
@@ -4,23 +4,23 @@ import React from 'react';
 import { routes } from 'router/routes';
 
 const App = () => {
-  return (
-      <Router>
-        <div className="router-container">
-          <Header />
-            <Switch>
-              {routes.map((route, i) => (
-                <Route
-                  key={i}
-                  path={route.path}
-                  exact={route.exact}
-                  component={route.component}
-                />
-              ))}
-            </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div className="router-container">
+                <Header />
+                <Switch>
+                    {routes.map((route, i) => (
+                        <Route
+                            key={i}
+                            path={route.path}
+                            exact={route.exact}
+                            component={route.component}
+                        />
+                    ))}
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
